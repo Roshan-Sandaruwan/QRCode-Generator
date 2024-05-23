ad2a154de8f3546ee092f7b93eb078788543a6c6
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-05-23 17:00:16 +0530

    Project is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import QRCode from "react-qr-code";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h2>QR Code <span>Generator </span> </h2>
 
+        <div className="qrcode-content">
+          <QRCode value={text} />
+        </div>
+
+        <div className="input">
+          <p>Enter QR value here</p>
+
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="text-content"
+          />
+        </div>
       </header>
     </div>
   );
